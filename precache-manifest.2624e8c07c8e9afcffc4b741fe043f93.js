a943ea32eca6479c724114631c07faecba47bbe2
von-dee <vondeesela@gmail.com>
2022-02-08 10:37:50 +0000

    Updates

---
+++ precache-manifest.2624e8c07c8e9afcffc4b741fe043f93.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03c47048dbe418af7d1ec9ad4419d42c",
+    "url": "/polywrap-tezos-demo/harbinger/index.html"
+  },
+  {
+    "revision": "14bb09e6da126e54a6f8",
+    "url": "/polywrap-tezos-demo/harbinger/static/js/2.1c83feec.chunk.js"
+  },
+  {
+    "revision": "cb9e5675730c4fc08b77",
+    "url": "/polywrap-tezos-demo/harbinger/static/js/main.fa065530.chunk.js"
+  },
+  {
+    "revision": "14476d849d60abb64ee5",
+    "url": "/polywrap-tezos-demo/harbinger/static/js/runtime-main.58ba6882.js"
+  }
+]);
